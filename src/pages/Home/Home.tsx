6afbd447735b0af758d9b57afbab5509c8878033
kareemegy <kokekarim@yahoo.com>
2023-06-03 21:02:32 +0300

    fix img errors

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,11 @@
 import cn from "classnames";
 import { CSSProperties } from "react";
-
+import Logo from "../../assets/images/Home Page/logo.png";
+import GDBImage from "../../assets/images/Home Page/GDB-desktop-main-LD.png";
+import ChervronRight from "../../assets/images/Home Page/Chevron right.svg";
 const Home = () => {
   const backgroundImageStyle: CSSProperties = {
-    backgroundImage:
-      'url("src/assets/images/Home Page/GDB-desktop-main-LD.png")',
+    backgroundImage: `url("${GDBImage}")`,
     backgroundSize: "cover",
     backgroundPosition: "center",
   };
@@ -15,11 +16,7 @@ const Home = () => {
           className="flex justify-center items-center  "
           style={backgroundImageStyle}
         >
-          <img
-            className="max-sm:hidden"
-            src="src/assets/images/Home Page/logo.png"
-            alt="Blink_Logo"
-          />
+          <img className="max-sm:hidden" src={Logo} alt="Blink_Logo" />
         </div>
         <div
           className={cn(
@@ -31,21 +28,20 @@ const Home = () => {
             <h1 className="font-bold text-4xl max-[996px]:text-2xl  leading-10">
               Hey [Mr. Bond],
             </h1>
-            <p className="text-gray-400 text-2xl max-[996px]:text-lg  mt-4 leading-6">
+            <p className="text-gray-400 text-2xl max-[996px]:text-lg max-sm:text-sm  mt-4 leading-6">
               We cordially invite you to [Event Name] in [Location].{" "}
             </p>
-            <p className="text-gray-400 text-2xl max-[996px]:text-lg  mt-4 leading-6">
+            <p className="text-gray-400 text-2xl max-[996px]:text-lg max-sm:text-sm   mt-4 leading-6">
               Please RSVP and register to confirm your attendance.
             </p>
-            <p className="mt-4 text-2xl underline">Read our security policy.</p>
-            <button className="flex justify-center items-center mt-5 font-bold text-2xl max-[996px]:text-lg  bg-white text-gray-800 px-7 py-5 rounded-sm w-full ">
+            <p className="mt-4 text-2xl max-[996px]:text-lg max-sm:text-sm underline   ">
+              Read our security policy.
+            </p>
+            <button className="flex justify-center items-center mt-5 font-bold text-2xl max-[996px]:text-lg  max-sm:text-sm  bg-white text-gray-800 px-7 py-5 rounded-sm w-full ">
               Continue{" "}
-              <span className="pl-4 pt-2">
+              <span className="pl-4 pt-2 max-sm:pt-0">
                 {" "}
-                <img
-                  src="src/assets/images/Home Page/Chevron right.svg"
-                  alt="Chevron right"
-                />
+                <img className="max-sm:text-sm" src={ChervronRight} alt="Chevron right" />
               </span>
             </button>
           </div>
