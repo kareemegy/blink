dc0a79e3fb069cf9a93e42047a2d58a71045e839
kareemegy <kokekarim@yahoo.com>
2023-06-14 15:01:42 +0300

    fix all done heading

--- src/pages/Notification/Notification.tsx
+++ src/pages/Notification/Notification.tsx
@@ -1,12 +1,18 @@
-import React from "react";
 import PageHeader from "../../components/PageHeader/PageHeader";
+import Wrapper from "../../components/Wrapper/Wrapper";
 
 const Notification = () => {
   return (
-    <div className="bg-black  h-screen">
+    <div className="bg-black h-screen">
       <div className="container mx-auto">
         <div className="">
           <PageHeader title="Notifications" className="mt-0" />
+          <Wrapper>
+            <div>
+              <span>Nov / 15 / 2022</span>
+              
+            </div>
+          </Wrapper>
         </div>
       </div>
     </div>
