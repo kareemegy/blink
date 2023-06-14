b88fcc1a00b78627c6a589bc03a756ca6fde0465
kareemegy <kokekarim@yahoo.com>
2023-06-14 23:15:59 +0300

    fix notes page white space

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,10 +1,15 @@
 import Logo from "../../assets/images/logo.png";
-const Footer = () => {
+interface FooterProps {
+  className?: string;
+}
+const Footer = ({ className }: FooterProps) => {
   return (
-    <div className="container md:hidden flex justify-between items-center bg-black h-28 text-white ">
+    <div
+      className={`container md:hidden flex justify-between items-center bg-black h-28 text-white ${className} `}
+    >
       <img className=" w-[200px]  " src={Logo} alt="Blink Logo" />
       <p className="text-sm w-[200px] ">
-        Copyright © 2022 bl:nk™. All Rights Reserved.
+        Copyright © 2023 bl:nk™. All Rights Reserved.
       </p>
     </div>
   );
--- src/pages/Notes/Notes.tsx
+++ src/pages/Notes/Notes.tsx
@@ -6,7 +6,7 @@ import PageHeader from "../../components/PageHeader/PageHeader";
 import ProgressBar from "../../components/ProgressBar/ProgressBar";
 import SelectLabel from "../../components/SelectLabel/SelectLabel";
 import Wrapper from "../../components/Wrapper/Wrapper";
-import Bar from "../../assets/images/bar.svg";
+import Bar from "../../assets/images/progressBar2.svg";
 import { useNavigate } from "react-router-dom";
 
 const Notes = () => {
@@ -15,38 +15,40 @@ const Notes = () => {
     navigate("/alldone");
   };
   return (
-    <div className="bg-black h-screen">
-      <Header />
-      <ProgressBar className="hidden md:block" type="allergies" />
-      <div className="container mx-auto">
-        <PageHeader
-          title="Notes"
-          subtitle="Let us know if there are any other notes you’d like us to know."
-        />
-      </div>
-      <Wrapper className=" mt-20">
-        <div>
-          <SelectLabel title="Other notes" className="text-Gray200" />
-          <Input
-            type="text"
-            id="notes"
-            placeholder="Type here..."
-            className="bg-inputBg"
+    <div className="h-screen bg-black flex flex-col">
+      <Header className="flex-none" />
+      <div className="flex-grow overflow-y-auto">
+        <div className="container mx-auto">
+          <ProgressBar className="hidden md:block" />
+          <PageHeader
+            title="Notes"
+            subtitle="Let us know if there are any other notes you’d like us to know."
           />
-          <Button
-            title="Next"
-            style="white"
-            className="w-full mt-10"
-            handleClicked={nextPage}
-          />
-          <img
-            className="md:hidden w-full mt-10 mb-4"
-            src={Bar}
-            alt="progress bar"
-          />
+          <Wrapper className="mt-20">
+            <div>
+              <SelectLabel title="Other notes" className="text-Gray200" />
+              <Input
+                type="text"
+                id="notes"
+                placeholder="Type here..."
+                className="bg-inputBg"
+              />
+              <Button
+                title="Next"
+                style="white"
+                className="w-full mt-10"
+                handleClicked={nextPage}
+              />
+            </div>
+          </Wrapper>
         </div>
-      </Wrapper>
-      <Footer />
+      </div>
+      <img
+        className="md:hidden w-full  px-10  "
+        src={Bar}
+        alt="progress bar"
+      />
+      <Footer className="flex-none" />
     </div>
   );
 };
